68e1fa1ffcc4c496b607965a4bc37b181bb134e9
humzah-77 <58381410+humzah-77@users.noreply.github.com>
2021-05-15 15:52:04 -0400

    Add files via upload

--- rfiddoorlock.ino
+++ rfiddoorlock.ino
@@ -52,11 +52,11 @@
  * -------------------------------------
  * -------------------------------------
  * -------------------------------------
- * SERVO       Arduino       
- * MOTOR      Uno/101       
+ * Relay       Arduino       
+ * Module      Uno/101       
  * Pin         Pin          
  * -------------------------------------
- * SG          8           
+ * S          8           
  * -------------------------------------
  * -------------------------------------
  * -------------------------------------
@@ -66,11 +66,7 @@
 #include <MFRC522.h>// allows the reading of frid tag ids
 #include "LinkedListLib.h"//allows creation of a linked list to store and maneuver through list of authorized uids
 #include <Servo.h>//allows communication with servo motor
-
-#define m1 25 //defined constants for uid of master card
-#define m2 178//second value 
-#define m3 200//third value 
-#define m4 115//fourth value
+#include <EEPROM.h>
 
 //all pins used
 #define SS_PIN 10
@@ -79,20 +75,20 @@ const int buzzer = 7;//buzzer pin
 int led = 6;//acces led light pin
 int master = 5;//mastercard led light pin
 int touch = 2;//touch sensor pin
-
+int lock = 8;
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-int pos = 0//position of servor;
-Servo myservo;  // create servo object to control a servo
+int pos = 0;//position of servor;
+int master1;
+int master2;
+int master3;
+int master4;
 
 boolean adding;//flags to turn on and off adding mode
 boolean sub = false;//flag to turn removing mode on and off
-
+boolean fix = false;
 byte nuidPICC[4];//this holds the previously scanned card
-LinkedList<byte> uid1;
-LinkedList<byte> uid2;//linked list for all 4 uid numbers
-LinkedList<byte> uid3;
-LinkedList<byte> uid4;
+
 
 int count;//count variable
 
@@ -100,29 +96,30 @@ int currentState; //the current state of the touch sensor
 int lastState; //the previous state of the touch sensor
 
 void setup() { 
+  master1 = EEPROM.read(1);
+  master2 = EEPROM.read(2);
+  master3 = EEPROM.read(3);
+  master4 = EEPROM.read(4);
   Serial.begin(9600);//begin serial communication 9600 baud rate
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
   count = 0;//initialize counter
   pos = 0;//initialize variable to tract servo position
   
-  myservo.attach(8);//attach servo object to associated servo pin
+  pinMode(lock, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(led, OUTPUT);//output pins 
   pinMode(master, OUTPUT);//set all pin modes 
   pinMode(touch, INPUT);//input pin
-  
-  myservo.write(pos);//set servo to 0 when starting 
 }
- 
+
 void loop() {
+  int curser = EEPROM.read(0);
   currentState = digitalRead(touch);//read current state of touch sensor
-  
   if (lastState == LOW && currentState == HIGH){//this if stament will only be true when the sensor switched from low state to a high state 
     opendoor();//lock/open the lock
   }
-  lastState = currentState;//recod the current state as the last state for the next loop
- 
+  else{}
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())//check if there is a card being scanned and if the card is readable by the frid reader
   {
     Serial.print(F("PICC type: "));
@@ -138,7 +135,7 @@ void loop() {
     }
    
     
-    if( nuidPICC[0] == m1 && nuidPICC[1] == m2 && nuidPICC[2] == m3 && nuidPICC[3] == m4)//checks if last scanned card id was the mastercard id 
+    if( nuidPICC[0] == master1 && nuidPICC[1] == master2 && nuidPICC[2] == master3 && nuidPICC[3] == master4)//checks if last scanned card id was the mastercard id 
     { 
       if (rfid.uid.uidByte[0] != nuidPICC[0] || //if it was check if current card id is not the same as master card id
         rfid.uid.uidByte[1] != nuidPICC[1] || 
@@ -157,49 +154,39 @@ void loop() {
       }
       else//if currently scanned card is also the master card meaning its been scanned twice in a row 
       {
-        if (rfid.uid.uidByte[0] == m1 && 
-        rfid.uid.uidByte[1] == m2 && 
-        rfid.uid.uidByte[2] == m3 && 
-        rfid.uid.uidByte[3] == m4 )
+        if (rfid.uid.uidByte[0] == master1 && 
+        rfid.uid.uidByte[1] == master2 && 
+        rfid.uid.uidByte[2] == master3 && 
+        rfid.uid.uidByte[3] == master4 )
         {
           sub = true; //then put program into removing mode
         }
       }
     }
     //if any other card id other than the master card id has been read then
-    
-    if(inhead(rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2],rfid.uid.uidByte[3]))//check if scanned card is in the head of the list 
+                             
+    if(rfid.uid.uidByte[0] == master1 && rfid.uid.uidByte[1] == master2 && rfid.uid.uidByte[2] == master3 && rfid.uid.uidByte[3] == master4)//if currently scanned card is master card 
     {
-      Serial.println(F("Open door"));//if it is open the door
-      opendoor();
-    }
-    else
-    {
-    /*if(intail(rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2],rfid.uid.uidByte[3])){//check if scanned card is in the tail of the list 
-        Serial.println(F("Open door"));//if it is open door 
-       digitalWrite(led, HIGH); // Turn the LED on
-       opendoor();  
-       }*/
-      if(inbody(rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2],rfid.uid.uidByte[3]))//check if scanned card id is in the body of the list 
-      {
-        Serial.println(F("open door"));//if it is open door
-        opendoor(); 
-      }
-    }
-                
-                  
-  
-    if(rfid.uid.uidByte[0] == m1 && rfid.uid.uidByte[1] == m2 && rfid.uid.uidByte[2] == m3 && rfid.uid.uidByte[3] == m4)//if currently scanned card is master card 
-    {
-      nuidPICC[0] = m1;
-      nuidPICC[1] = m2;//make previously scanned card varable into marster card id
-      nuidPICC[2] = m3;
-      nuidPICC[3] = m4;
+      nuidPICC[0] = master1;
+      nuidPICC[1] = master2;//make previously scanned card varable into marster card id
+      nuidPICC[2] = master3;
+      nuidPICC[3] = master4;
       Serial.println(F("mastercard scanned"));
       digitalWrite(master, HIGH); // Turn the martercard LED on
       delay(100);//100 milisecond delay
       digitalWrite(master, LOW); // Turn the mastercard LED off
     }
+    for(int i = 5; i < curser; i += 4){
+      if (rfid.uid.uidByte[0] == EEPROM.read(i) && 
+        rfid.uid.uidByte[1] == EEPROM.read(i+1) && 
+        rfid.uid.uidByte[2] == EEPROM.read(i+2) && 
+        rfid.uid.uidByte[3] == EEPROM.read(i+3) )
+        {
+          Serial.println(F("Open door"));//if it is open the door
+          opendoor();
+        }
+    }
+    lastState = currentState;//recod the current state as the last state for the next loop
     Serial.println(F("The NUID tag is:"));//show info about scanned card
     Serial.print(F("In hex: "));
     printHex(rfid.uid.uidByte, rfid.uid.size);//uid in hex format
@@ -223,50 +210,18 @@ void loop() {
 void opendoor()//this function opens and locks the lock
 {
   digitalWrite(led, HIGH); // Turn the access LED on
-  if(pos == 0){// if the lock is closed
-    pos = 150;//then set the next position to open 
-    }
-  else{//if the lock is open
-    pos = 0; //then set the new position to lock  
-  }
-  myservo.write(pos);//change the servo to its new location
+  digitalWrite(lock, HIGH);
   tone(buzzer, 400); //ring the buzzer 
   delay(500);//wait 500 miliseconds
   noTone(buzzer);//end buzzer
   delay(500);//wait another 500 milisecond
   digitalWrite(led, LOW); // Turn the access LED on
+  delay(3000);
+  digitalWrite(lock,LOW);
 }
 
 
-boolean inhead(byte id1, byte id2, byte id3, byte id4)//check if id is in head of linked list
-{
-  if(uid1.GetHead() == id1 && uid2.GetHead() == id2 && uid3.GetHead() == id3 && uid4.GetHead() == id4){//check if uid is in head of list 
-          return true;//if it is return true
-  }
-  else{
-    return false;// if not return false
-  }
-}
 
-boolean intail(byte id1, byte id2, byte id3, byte id4)//checks if id is in tail of linked list
-{
-  if(uid1.GetTail() == id1 && uid2.GetTail() == id2 && uid3.GetTail() == id3 && uid4.GetTail() == id4){//check if uid is in tail of list 
-          return true;//if it is return true
-  }
-  else{
-    return false;//if not return false
-  }
-}
-boolean inbody(byte id1, byte id2, byte id3, byte id4)//checks if id is in body of linked list
-{
-    for(int i = 0; i <= uid1.GetSize(); i++){
-       if(uid1.GetAt(i) == id1 && uid2.GetAt(i) == id2 && uid3.GetAt(i) == id3 && uid4.GetAt(i) == id4){//check if uid is in body of list
-             return true;//if it is return true
-        }         
-                
-    }
-    return false;//if not return false
-}
 
 void addtolist(byte id1, byte id2, byte id3, byte id4)//add a uid to the list 
 {
@@ -275,50 +230,39 @@ void addtolist(byte id1, byte id2, byte id3, byte id4)//add a uid to the list
         nuidPICC[1] = 0; 
         nuidPICC[2] = 0; //clear previously read card so it can be reassigned 
         nuidPICC[3] = 0;
-        if(inhead(id1, id2, id3,id4))//check if id is in head
-        {
-          Serial.println(F("already in list"));//if it is return
-          adding = true;
-          return;
-                }
-         else{
-           adding = false;// if its not then make adding list false
-         }
-         if(intail(id1, id2, id3,id4))//check if in list 
-         {             
-            Serial.println(F("already in list"));//if it is then leave 
+        adding = false;
+        if(EEPROM.read(0) > 5){
+          for(int i = 5; i < EEPROM.read(0); i += 4){
+          if (id1 == EEPROM.read(i) && 
+            id2 == EEPROM.read(i+1) && 
+            id3 == EEPROM.read(i+2) && 
+            id4 == EEPROM.read(i+3) )
+            {
             adding = true;
-            return;
                 }
-          else{
-             adding = false;//if not then make adding list false
-                    }
-            for(int i = 0; i < uid1.GetSize(); i++){//check if it is in the body of the list
-              if(uid1.GetAt(i) == id1 && uid2.GetAt(i) == id2 && uid3.GetAt(i) == id3 && uid4.GetAt(i) == id4){
-                  Serial.println(F("already in list"));//if it is then leave 
-                  adding = true;
-                  return;
-                }
-                else{
-                      adding = false;//if not then make adding false
-                    }
-                
             }
+        }
+        else{
+          adding = false;
+        }
             if(adding == false){// if not in list then add to list 
-              uid1.InsertHead(id1);//insert each id number into its respective list
-              uid2.InsertHead(id2);
-              uid3.InsertHead(id3);
-              uid4.InsertHead(id4);
+              int x = EEPROM.read(0);
+              EEPROM.write(0,x+4);
+              EEPROM.write(x,id1);//insert each id number into its respective list
+              EEPROM.write(x+1,id2);
+              EEPROM.write(x+2,id3);
+              EEPROM.write(x+3,id4);
             }
         
         count++;//incerement counter
         Serial.println();
      
-        for(int i = 0; i < uid1.GetSize(); i++){//show list of authorised ids in serial moniter
-          Serial.print(uid1.GetAt(i));
-          Serial.print(uid2.GetAt(i));//display the list
-          Serial.print(uid3.GetAt(i));
-          Serial.println(uid4.GetAt(i));
+        for(int i = 5; i < EEPROM.read(0); i += 4){//show list of authorised ids in serial moniter
+          Serial.print( EEPROM.read(i));
+          Serial.print( EEPROM.read(i + 1));//display the list
+          Serial.print( EEPROM.read(i + 2));
+          Serial.println( EEPROM.read(i + 3));
+          Serial.println( EEPROM.read(0));
         }
    
         Serial.println();
@@ -326,57 +270,89 @@ void addtolist(byte id1, byte id2, byte id3, byte id4)//add a uid to the list
 
 void removefromlist(byte id1, byte id2, byte id3, byte id4)//removes a uid from the list 
 {
-  Serial.println(F("removing"));
-  if(inhead(id1, id2, id3,id4))//if it is in head then remove
-  {
-    uid1.RemoveHead();
-    uid2.RemoveHead();
-    uid3.RemoveHead();
-    uid4.RemoveHead();
-    Serial.println(F("removed"));
-    sub = false;//turn off removing mode
-  }
-  else
-  {
-  Serial.println(F("not in list"));
-  sub = false;//if not in list turn off removing mode and return
-  }
-  if(intail(id1, id2, id3,id4)) //check if in list if it is remove it
-  {       
-    uid1.RemoveTail();
-    uid2.RemoveTail();
-    uid3.RemoveTail();
-    uid4.RemoveTail();
-    Serial.println(F("removed"));
-    sub = false;//turn off removing mode
-  }
-  else
-  {
-    Serial.println(F("not in list"));
-    sub = false;//if not in list turn off removing mode and return
-  }
-  for(int i = 0; i < uid1.GetSize(); i++)
-  {
-    if(uid1.GetAt(i) == rfid.uid.uidByte[0] && uid2.GetAt(i) == rfid.uid.uidByte[1] && uid3.GetAt(i) ==rfid.uid.uidByte[2] && uid4.GetAt(i) == rfid.uid.uidByte[3])//check if id is in bpdy if it is then remove it
-    {
-      uid1.RemoveAt(i);
-      uid2.RemoveAt(i);
-      uid3.RemoveAt(i);
-      uid4.RemoveAt(i);
-      Serial.println(F("removed"));
-      sub = false;//turn off removing mode
+  LinkedList<byte> uid1;
+  LinkedList<byte> uid2;//linked list for all 4 uid numbers
+  LinkedList<byte> uid3;
+  LinkedList<byte> uid4;
+  int z = 0;
+  for(int i = 5; i <= EEPROM.read(0); i+=4){
+    uid1.InsertHead(EEPROM.read(i));
+    uid2.InsertHead(EEPROM.read(i + 1));
+    uid3.InsertHead(EEPROM.read(i + 2));
+    uid4.InsertHead(EEPROM.read(i + 3));
     }
-    else
-    {
-      Serial.println(F("not in list"));
-      sub = false;//if not in list turn off removing mode and return
-    }             
+    for(int i = 0; i <= uid1.GetSize(); i++){
+       if(uid1.GetAt(i) == id1 && uid2.GetAt(i) == id2 && uid3.GetAt(i) == id3 && uid4.GetAt(i) == id4){//check if uid is in body of list
+             uid1.RemoveAt(i);
+             uid2.RemoveAt(i);
+             uid3.RemoveAt(i);
+             uid4.RemoveAt(i);
+             int cerser = EEPROM.read(0);
+             cerser -= 4;
+             for(int i = 5; i < EEPROM.read(0); i+=4){
+              EEPROM.write(i,0);
+              EEPROM.write(i + 1,0);
+              EEPROM.write(i + 2,0);
+              EEPROM.write(i + 3,0);
+            }
+            EEPROM.update(0,cerser);
+            for(int i = 5; i < EEPROM.read(0); i+=4){
+              EEPROM.update(i,uid1.GetAt(z));
+              EEPROM.update(i + 1,uid2.GetAt(z));
+              EEPROM.update(i + 2,uid3.GetAt(z));
+              EEPROM.update(i + 3,uid4.GetAt(z));
+              z++;
+            }
+            for(int i = 5; i < EEPROM.read(0); i += 4){//show list of authorised ids in serial moniter
+              Serial.print( EEPROM.read(i));
+              Serial.print( EEPROM.read(i + 1));//display the list
+              Serial.print( EEPROM.read(i + 2));
+              Serial.println( EEPROM.read(i + 3));
+              Serial.println( EEPROM.read(0));
+            }
+       
+            Serial.println();
+       }         
+                
+    }
+  /*Serial.println(F("removing"));
+    for(int i = 5; i < EEPROM.read(0); i += 4){
+          if (id1 == EEPROM.read(i) && 
+            id2 == EEPROM.read(i+1) && 
+            id3 == EEPROM.read(i+2) && 
+            id4 == EEPROM.read(i+3) )
+            {
+            EEPROM.write(i,0);
+            EEPROM.write(i+1,0);
+            EEPROM.write(i+1,0);
+            EEPROM.write(i+3,0);
+            if(i+4 < EEPROM.read(0)){
+              
+            }
+            }
+        if(fix){
+          int m1 = EEPROM.read(i) ;
+          int m2 = EEPROM.read(i +1);
+          int m3 = EEPROM.read(i+2);
+          int m4 = EEPROM.read(i + 3);   
+          EEPROM.update(i-4,m1);
+          EEPROM.update(i-3,m2);
+          EEPROM.update(i-2,m3);
+          EEPROM.update(i-1,m4);
+          
+        }
+  }
+  fix = false;
+  int y = EEPROM.read(0);
+  if (y>5){ 
+    y = y - 4;
+    EEPROM.update(0,y); 
   }
   nuidPICC[0] = 0;//reset last read card to be reassigned a value
   nuidPICC[1] = 0; 
   nuidPICC[2] = 0; 
   nuidPICC[3] = 0;
-  sub = false;//turn off removing mode
+  sub = false;//turn off removing mode*/
   }
 /**
  * Helper routine to dump a byte array as hex values to Serial. 
